eb371c55e4c3fffe0d3a32663f593e899d01d395
JefersonCalderon <jcalderon-2019036@kinal.edu.gt>
2021-08-25 11:58:22 -0600

    Creo la pagina de inicio, el about y la barra de navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componetes/inicio/inicio.component';
+import { LoginComponent } from './componetes/login/login.component';
+import { MenudenavegacionComponent } from './componetes/menudenavegacion/menudenavegacion.component';
+import { PrincipalComponent } from './componetes/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "menudenavegacion", component: MenudenavegacionComponent},
+  {path: "inicio", component: InicioComponent},
+  {path: "iniciarsesion", component: LoginComponent},
+  {path: "principal", component: PrincipalComponent},
+  {path: "**", component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenudenavegacionComponent } from './componetes/menudenavegacion/menudenavegacion.component';
+import { InicioComponent } from './componetes/inicio/inicio.component';
+import { LoginComponent } from './componetes/login/login.component';
+import { PrincipalComponent } from './componetes/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenudenavegacionComponent,
+    InicioComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
